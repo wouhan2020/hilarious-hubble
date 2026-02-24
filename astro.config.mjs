7f7bfed0a0bc2968c54944cb5ea26f4dce387c68
K r <uhanskam2@gmx.fr>
2026-02-24 16:51:48 +0100

    pousse sur github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Remplace par ton URL réelle
+    site: 'https://wouhan2020.github.io/hilarious-hubble', 
+    // Si ton dépôt s'appelle "mon-site", ajoute base: '/mon-site'
+  });
